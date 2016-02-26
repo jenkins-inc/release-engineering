ad437c5148bf36a7e7ca912dc9d458baf28b1c75
Andrew Bayer <andrew.bayer@gmail.com>
2016-02-26 15:22:24 -0800

    Added a deployment step

--- vars/microService.groovy
+++ vars/microService.groovy
@@ -7,5 +7,9 @@ def call() {
     node {
         checkout scm
         sh "${tool 'Maven 3.x'}/bin/mvn install"
+        switch (env.BRANCH_NAME) {
+        case 'master':
+            sh "heroku deploy:war --war target/*.war --app borat-dev"
+        }
     }
 }
