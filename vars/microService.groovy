97d6ab0aa4963e2c62b46484753e3b831fa95cf5
Kohsuke Kawaguchi <kk@kohsuke.org>
2016-02-27 16:47:27 -0800

    Adding the test stage and promotion stage

--- vars/microService.groovy
+++ vars/microService.groovy
@@ -16,6 +16,23 @@ def call() {
         if (branch in ['dev','staging','prod']) {
             stage 'Deploy'
             sh "heroku deploy:war --war target/*.war --app ${repoName}-${branch}"
+
+            stage 'Test'
+            sh 'sleep 300' // this is where the actual test would have gone
+
+            stage 'Promote'
+            switch (branch) {
+            case 'dev':
+                mergeTo('staging')
+            case 'staging':
+                mergeTo('prod')
+            }
         }
     }
+}
+
+def mergeTo(target) {
+    sh "git checkout ${target}"
+    sh "git reset --hard origin/${target}" // move to the current tip
+    sh "git merge ${env.BRANCH_NAME}"
 }
