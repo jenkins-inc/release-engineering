876fb9cc0561f90fd0a758b3f272e195ec20901b
Kohsuke Kawaguchi <kk@kohsuke.org>
2016-02-28 08:42:58 -0800

    Still fiddling with the script

--- vars/microService.groovy
+++ vars/microService.groovy
@@ -18,7 +18,7 @@ def call() {
             sh "heroku deploy:war --war target/*.war --app ${repoName}-${branch}"
 
             stage 'Test'
-            sh 'sleep 300' // this is where the actual test would have gone
+            sh 'sleep 5' // this is where the actual test would have gone
 
             stage 'Promote'
             switch (branch) {
@@ -34,7 +34,7 @@ def call() {
 def mergeTo(target) {
     sh "git checkout ${target}"
     sh "git reset --hard origin/${target}" // move to the current tip
-    sh "git merge ${env.BRANCH_NAME}"
+    sh "git merge origin/${env.BRANCH_NAME}"
     sh "git commit -m 'promotion by Jenkins'"
     sh "git push ${target}"
 }
