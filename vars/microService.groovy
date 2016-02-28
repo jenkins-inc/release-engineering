40de9beccc7162c142599c6edb2a4cca497db099
Kohsuke Kawaguchi <kk@kohsuke.org>
2016-02-28 08:46:04 -0800

    In case there was no change

--- vars/microService.groovy
+++ vars/microService.groovy
@@ -35,6 +35,6 @@ def mergeTo(target) {
     sh "git checkout ${target}"
     sh "git reset --hard origin/${target}" // move to the current tip
     sh "git merge origin/${env.BRANCH_NAME}"
-    sh "git commit -m 'promotion by Jenkins'"
+    sh "git commit --allow-empty -m 'promotion by Jenkins'"
     sh "git push ${target}"
 }
